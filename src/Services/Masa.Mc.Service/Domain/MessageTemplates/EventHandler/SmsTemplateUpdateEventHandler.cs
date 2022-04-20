c887b79893bb20554f2f3a4b4c36a6abe27915b8
吴自浩 <wuzihao425@outlook.com>
2022-04-20 09:27:28 +0800

    refactor：add smsChannelChangedDomainEvent

---
+++ src/Services/Masa.Mc.Service/Domain/MessageTemplates/EventHandler/SmsTemplateUpdateEventHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Masa.Mc.Service.Admin.Domain.MessageTemplates.EventHandler;
+
+public class SmsTemplateUpdateEventHandler
+{
+    [EventHandler]
+    public void HandleEvent(SmsChannelChangedDomainEvent @event)
+    {
+        //After the SMS channel is changed, query the Alibaba cloud SMS interface and synchronize the SMS template
+    }
+}
