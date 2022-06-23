7716b093a331f0f1260b0eb45d5f2c3af30b0a1b
吴自浩 <wuzihao425@outlook.com>
2022-06-23 14:40:04 +0800

    style：adjust message center style

--- src/Contracts/Masa.Mc.Contracts.Admin/Dtos/MessageInfos/Validator/MessageInfoUpsertDtoValidator.cs
+++ src/Contracts/Masa.Mc.Contracts.Admin/Dtos/MessageInfos/Validator/MessageInfoUpsertDtoValidator.cs
@@ -7,7 +7,7 @@ public class MessageInfoUpsertDtoValidator : AbstractValidator<MessageInfoUpsert
 {
     public MessageInfoUpsertDtoValidator()
     {
-        RuleFor(dto => dto.Title).Required().Length(2, 255);
+        RuleFor(dto => dto.Title).Required().Length(2, 50);
         RuleFor(dto => dto.Content).Required();
         RuleFor(dto => dto.JumpUrl).Required().Url().When(m => m.IsJump);
     }
