04aed12dd11736110f31229c17f45e56d0fa03c5
wzh425 <wuzihao425@outlook.com>
2022-07-08 14:56:53 +0800

    feat：add mailkit support for email sending

---
+++ src/Infrastructure/Masa.Mc.Infrastructure.MailKit/_Imports.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+global using MailKit.Security;
+global using MailKit.Net.Smtp;
+global using Masa.Mc.Infrastructure.Email;
+global using Masa.Mc.Infrastructure.Email.Infrastructure.OptionsResolve;
+global using System.Net.Mail;
+global using Microsoft.Extensions.Options;
+global using MimeKit;
+global using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+global using Masa.Mc.Infrastructure.Email.Infrastructure.OptionsResolve.Contributors;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
