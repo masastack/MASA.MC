15efbca2c9aaf293164e8199de610f9dd8b97850
wzh425 <wuzihao425@outlook.com>
2022-07-15 12:57:38 +0800

    refactor：SchedulerJobAppConfig change

--- src/Services/Masa.Mc.Service/Domain/MessageTasks/EventHandler/ResolveMessageTaskEventHandler.cs
+++ src/Services/Masa.Mc.Service/Domain/MessageTasks/EventHandler/ResolveMessageTaskEventHandler.cs
@@ -166,7 +166,7 @@ public class ResolveMessageTaskEventHandler
             {
                 JobAppIdentity = MessageTaskExecuteJobConsts.JOB_APP_IDENTITY,
                 JobEntryAssembly = MessageTaskExecuteJobConsts.JOB_ENTRY_ASSEMBLY,
-                JobEntryMethod = MessageTaskExecuteJobConsts.JOB_ENTRY_METHOD,
+                JobEntryClassName = MessageTaskExecuteJobConsts.JOB_ENTRY_METHOD,
                 JobParams = eto.MessageTaskId.ToString(),
             }
         };
