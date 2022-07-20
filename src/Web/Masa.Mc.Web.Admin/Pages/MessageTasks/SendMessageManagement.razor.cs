5fa503a5dc04f67a63246cbccdb87fe164fe7f5c
wzh425 <wuzihao425@outlook.com>
2022-07-20 17:43:55 +0800

    fix：issues 176

---
+++ src/Web/Masa.Mc.Web.Admin/Pages/MessageTasks/SendMessageManagement.razor.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Mc.Web.Admin.Pages.MessageTasks;
+
+public partial class SendMessageManagement : AdminCompontentBase
+{
+    private SendMessage _sendMessage = default!;
+    private MessageTaskList _messageTaskList = default!;
+    private bool _isSend = true;
+}
