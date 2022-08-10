08230768e2ad8e053adb66dd8f7afb85a2e82dc1
wzh425 <wuzihao425@outlook.com>
2022-08-10 16:43:35 +0800

    refactor：Reconstruction message record user display

---
+++ src/Contracts/Masa.Mc.Contracts.Admin/Dtos/MessageRecords/MessageRecordUserDto.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Mc.Contracts.Admin.Dtos.MessageRecords;
+
+public class MessageRecordUserDto
+{
+    public string DisplayName { get; set; } = string.Empty;
+
+    public string Account { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Avatar { get; set; } = string.Empty;
+}
--- src/Services/Masa.Mc.Service/Domain/MessageRecords/EventHandler/UpdateMessageRecordUserEventHandler.cs
+++ src/Services/Masa.Mc.Service/Domain/MessageRecords/EventHandler/UpdateMessageRecordUserEventHandler.cs
@@ -22,6 +22,7 @@ public class UpdateMessageRecordUserEventHandler
     public async Task HandleEventAsync(UpdateMessageRecordUserEvent eto)
     {
         var messageRecord = await _repository.FindAsync(x => x.Id == eto.MessageRecordId);
+
         if (messageRecord == null)
         {
             _logger.LogInformation($"UpdateMessageRecordUserEventHandler:messageRecords is null");
@@ -29,9 +30,14 @@ public class UpdateMessageRecordUserEventHandler
         }
 
         var user = await GetMessageRecordsUser(messageRecord);
+
         if (user == null)
         {
-            _logger.LogInformation($"UpdateMessageRecordUserEventHandler:user is null");
+            user = await CreateExternalUserAsync(messageRecord);
+        }
+
+        if (user == null)
+        {
             return;
         }
 
@@ -53,18 +59,60 @@ public class UpdateMessageRecordUserEventHandler
 
     private async Task<UserModel?> GetMessageRecordsUser(MessageRecord messageRecords)
     {
+        try
+        {
+            if (messageRecords.Channel.Type == ChannelTypes.Sms)
+            {
+                var phoneNumber = messageRecords.GetDataValue<string>(nameof(MessageReceiverUser.PhoneNumber));
+                return await _authClient.UserService.FindByPhoneNumberAsync(phoneNumber);
+            }
+            else if (messageRecords.Channel.Type == ChannelTypes.Email)
+            {
+                var email = messageRecords.GetDataValue<string>(nameof(MessageReceiverUser.Email));
+                return await _authClient.UserService.FindByEmailAsync(email);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "UpdateMessageRecordUserEventHandler");
+            return null;
+        }
+    }
+
+    private async Task<UserModel?> CreateExternalUserAsync(MessageRecord messageRecords)
+    {
+        var addUserModel = new AddUserModel();
         if (messageRecords.Channel.Type == ChannelTypes.Sms)
         {
             var phoneNumber = messageRecords.GetDataValue<string>(nameof(MessageReceiverUser.PhoneNumber));
-            return await _authClient.UserService.FindByPhoneNumberAsync(phoneNumber);
+            addUserModel.Account = phoneNumber;
+            addUserModel.DisplayName = phoneNumber;
+            addUserModel.Password = phoneNumber;
         }
         else if (messageRecords.Channel.Type == ChannelTypes.Email)
         {
             var email = messageRecords.GetDataValue<string>(nameof(MessageReceiverUser.Email));
-            return await _authClient.UserService.FindByEmailAsync(email);
+            addUserModel.Account = email;
+            addUserModel.DisplayName = email;
+            addUserModel.Email = email;
         }
         else
         {
+            return null;
+        }
+
+        try
+        {
+            var user = await _authClient.UserService.AddAsync(addUserModel);
+            return user;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "UpdateMessageRecordUserEventHandler");
             return null;
         }
     }
