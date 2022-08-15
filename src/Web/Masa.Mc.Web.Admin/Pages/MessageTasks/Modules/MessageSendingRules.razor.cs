64a23febffc01154c9cbec8be40c686935c1e886
wzh425 <wuzihao425@outlook.com>
2022-08-15 09:23:41 +0800

    style:Move it to _Imports.

--- src/Web/Masa.Mc.Web.Admin/Pages/MessageTasks/Modules/MessageSendingRules.razor.cs
+++ src/Web/Masa.Mc.Web.Admin/Pages/MessageTasks/Modules/MessageSendingRules.razor.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Blazor.Presets;
-
 namespace Masa.Mc.Web.Admin.Pages.MessageTasks.Modules;
 
 public partial class MessageSendingRules : AdminCompontentBase
