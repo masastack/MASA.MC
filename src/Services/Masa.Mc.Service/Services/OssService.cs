c9524e39c1989d3f4af081b5c87cce89f31b7d54
wzh425 <wuzihao425@outlook.com>
2022-12-23 17:15:46 +0800

    fix : Failed to repair SSO file upload

--- src/Services/Masa.Mc.Service/Services/OssService.cs
+++ src/Services/Masa.Mc.Service/Services/OssService.cs
@@ -7,17 +7,17 @@ public class OssService : ServiceBase
 {
     public OssService(IServiceCollection services) : base("api/oss")
     {
-        MapGet(GetSecurityTokenAsync);
+        
     }
 
-    private async Task<GetSecurityTokenDto> GetSecurityTokenAsync([FromServices] IClient client, [FromServices] DaprClient daprClient)
+    public async Task<SecurityTokenDto> GetSecurityTokenAsync([FromServices] IClient client, [FromServices] IOptions<OssOptions> ossOptions)
     {
         var region = "oss-cn-hangzhou";
         var response = client.GetSecurityToken();
         var stsToken = response.SessionToken;
         var accessId = response.AccessKeyId;
         var accessSecret = response.AccessKeySecret;
-        var bucket = (await daprClient.GetSecretAsync("localsecretstore", "ali-masa-cdn-dev"))["bucket"];
-        return await Task.FromResult(new GetSecurityTokenDto(region, accessId, accessSecret, stsToken, bucket));
+        var bucket = ossOptions.Value.Bucket;
+        return await Task.FromResult(new SecurityTokenDto(region, accessId, accessSecret, stsToken, bucket));
     }
 }
