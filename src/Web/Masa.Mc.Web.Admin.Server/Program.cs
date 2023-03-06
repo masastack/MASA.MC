d6f570d4a7a8c853e60eab4ff43610f220f2bf08
wzh425 <wuzihao425@outlook.com>
2023-03-06 17:32:30 +0800

    chore : Change the reading method of TlsName

--- src/Web/Masa.Mc.Web.Admin.Server/Program.cs
+++ src/Web/Masa.Mc.Web.Admin.Server/Program.cs
@@ -13,7 +13,7 @@ builder.WebHost.UseKestrel(option =>
 {
     option.ConfigureHttpsDefaults(options =>
     {
-        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TLS_NAME")))
+        if (string.IsNullOrEmpty(masaStackConfig.TlsName))
         {
             options.ServerCertificate = new X509Certificate2(Path.Combine("Certificates", "7348307__lonsid.cn.pfx"), "cqUza0MN");
         }
