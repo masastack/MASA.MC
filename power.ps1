ca554cee252168b56eef463217565b513f7f11fa
wzh425 <wuzihao425@outlook.com>
2023-03-27 14:49:40 +0800

    chore : Remove Ignore (#396)

---
+++ power.ps1
@@ -0,0 +1,18 @@
+param($t,$u,$p)
+
+#docker build 
+#Write-Host "Hello.$args"
+Write-Host "Hello $t"
+
+docker login --username=$u registry.cn-hangzhou.aliyuncs.com --password=$p
+
+$ServiceDockerfilePath="./src/Services/Masa.Mc.Service/Dockerfile"
+$ServiceServerName="masa-mc-service"
+$WebDockerfilePath="./src/Web/Masa.Mc.Web.Admin.Server/Dockerfile"
+$WebServerName="masa-mc-ui-admin"
+
+docker build -t registry.cn-hangzhou.aliyuncs.com/masastack/${ServiceServerName}:$t  -f $ServiceDockerfilePath .
+docker push registry.cn-hangzhou.aliyuncs.com/masastack/${ServiceServerName}:$t 
+
+docker build -t registry.cn-hangzhou.aliyuncs.com/masastack/${WebServerName}:$t  -f $WebDockerfilePath .
+docker push registry.cn-hangzhou.aliyuncs.com/masastack/${WebServerName}:$t 
