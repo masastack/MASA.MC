26ff329b9fc104994e69aeba2c32104db1bd6403
yazhouzhang <y_az@163.com>
2023-03-31 17:53:50 +0800

    Fix: Message record, advanced search, Select component text is trunca… (#405)
    * Fix: Message record, advanced search, Select component text is truncated. After successfully adding an external member, when you add it again, opening a popover immediately triggers form validation. Look up prompt i18n (Modal dialog)
    
    * Modify form validator tips message

--- src/Contracts/Masa.Mc.Contracts.Admin/Dtos/Subjects/Validator/CreateExternalUserDtoValidator.cs
+++ src/Contracts/Masa.Mc.Contracts.Admin/Dtos/Subjects/Validator/CreateExternalUserDtoValidator.cs
@@ -7,9 +7,9 @@ public class CreateExternalUserDtoValidator : AbstractValidator<CreateExternalUs
 {
     public CreateExternalUserDtoValidator()
     {
-        RuleFor(inputDto => inputDto.DisplayName).Required("DisplayNameRequired").Length(2, 50).ChineseLetterNumber();
-        RuleFor(inputDto => inputDto.PhoneNumber).Required("PhoneNumberRequired").Phone().When(u => string.IsNullOrEmpty(u.Email) || !string.IsNullOrEmpty(u.PhoneNumber));
-        RuleFor(inputDto => inputDto.Email).Required("EmailRequired")
+        RuleFor(inputDto => inputDto.DisplayName).Required("UserDisplayNameRequired").Length(2, 50).WithMessage("UserDisplayNameLength").ChineseLetterNumber().WithMessage("UserDisplayNameChineseLetterNumber");
+        RuleFor(inputDto => inputDto.PhoneNumber).Required().Phone().When(u => string.IsNullOrEmpty(u.Email) || !string.IsNullOrEmpty(u.PhoneNumber));
+        RuleFor(inputDto => inputDto.Email).Required()
             .Email().WithMessage("EmailInvalid")
             .When(u => string.IsNullOrEmpty(u.PhoneNumber) || !string.IsNullOrEmpty(u.Email));
     }
