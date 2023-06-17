ffa2044a4fb2a007814d9c33ce5e22d57f872707
wzh425 <wuzihao425@outlook.com>
2023-06-17 10:28:09 +0800

    reactor : Send message SimpleModal to SheetDialog

---
+++ src/Web/Masa.Mc.Web.Admin/Components/Default/DefaultSheetDialog.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Mc.Web.Admin.Components.Default;
+
+public class DefaultSheetDialog : SSheetDialog
+{
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        EnableDomReload = true;
+        ContentClass ??= "";
+        if (ContentClass.Contains("sheetDialogPadding") is false)
+            ContentClass += " sheetDialogPadding";
+    }
+}
