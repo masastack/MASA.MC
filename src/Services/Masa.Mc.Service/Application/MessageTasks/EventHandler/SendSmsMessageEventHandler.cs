2954c59bb96d075c709bf66bd86513df85173498
wzh425 <wuzihao425@outlook.com>
2023-06-17 15:21:16 +0800

    style : global using

--- src/Services/Masa.Mc.Service/Application/MessageTasks/EventHandler/SendSmsMessageEventHandler.cs
+++ src/Services/Masa.Mc.Service/Application/MessageTasks/EventHandler/SendSmsMessageEventHandler.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Mc.Service.Admin.Domain.MessageRecords.Aggregates;
-
 namespace Masa.Mc.Service.Admin.Application.MessageTasks.EventHandler;
 
 public class SendSmsMessageEventHandler
