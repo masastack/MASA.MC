45a116679697a28d124624b109078c89a20e54f9
wzh425 <wuzihao425@outlook.com>
2024-07-23 14:50:05 +0800

    style: remove using

--- src/Web/Masa.Mc.Web.Admin/Pages/MessageTemplates/WeixinWorkTemplateManagement.razor.cs
+++ src/Web/Masa.Mc.Web.Admin/Pages/MessageTemplates/WeixinWorkTemplateManagement.razor.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using System.Reflection;
-
 namespace Masa.Mc.Web.Admin.Pages.MessageTemplates;
 
 public partial class WeixinWorkTemplateManagement : AdminCompontentBase
