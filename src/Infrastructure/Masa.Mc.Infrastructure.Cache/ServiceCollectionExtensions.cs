14f7d4fe63646487e75e2095aa9b3fbec2a71e5c
wzh425 <wuzihao425@outlook.com>
2024-09-13 18:48:59 +0800

    fix: Redis Options not specified (#597)

--- src/Infrastructure/Masa.Mc.Infrastructure.Cache/ServiceCollectionExtensions.cs
+++ src/Infrastructure/Masa.Mc.Infrastructure.Cache/ServiceCollectionExtensions.cs
@@ -2,9 +2,9 @@
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddCache(this IServiceCollection services)
+    public static IServiceCollection AddCache(this IServiceCollection services, RedisConfigurationOptions redisOptions)
     {
-        services.AddMultilevelCache(options => options.UseStackExchangeRedisCache());
+        services.AddMultilevelCache(options => options.UseStackExchangeRedisCache(redisOptions));
         services.AddScoped<ICacheContext, CacheContext>();
         return services;
     }
