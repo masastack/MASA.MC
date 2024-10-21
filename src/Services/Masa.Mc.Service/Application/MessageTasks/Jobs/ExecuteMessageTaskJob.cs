270a4aca728514a192072014f758990a6da23ae9
wzh425 <wuzihao425@outlook.com>
2024-10-21 10:30:33 +0800

    🆕 feat: Optimize message task sending

--- src/Services/Masa.Mc.Service/Application/MessageTasks/Jobs/ExecuteMessageTaskJob.cs
+++ src/Services/Masa.Mc.Service/Application/MessageTasks/Jobs/ExecuteMessageTaskJob.cs
@@ -16,57 +16,20 @@ public class ExecuteMessageTaskJob : BackgroundJobBase<ExecuteMessageTaskJobArgs
     protected override async Task ExecutingAsync(ExecuteMessageTaskJobArgs args)
     {
         await using var scope = _serviceProvider.CreateAsyncScope();
-        var (channelRepository, messageTaskRepository, messageTaskHistoryRepository, eventBus, domainService, messageTaskJobService, unitOfWork) = await GetRequiredServiceAsync(scope.ServiceProvider, args.Environment);
+        var multiEnvironmentSetter = scope.ServiceProvider.GetRequiredService<IMultiEnvironmentSetter>();
+        multiEnvironmentSetter.SetEnvironment(args.Environment);
+        var eventBus = scope.ServiceProvider.GetRequiredService<IEventBus>();
 
         var activity = string.IsNullOrEmpty(args.TraceParent) ? default : MessageTaskExecuteJobConsts.ActivitySource.StartActivity("", ActivityKind.Consumer, args.TraceParent);
 
         try
         {
-            var history = await messageTaskHistoryRepository.FindWaitSendAsync(args.MessageTaskId, args.IsTest);
-
-            if (history == null)
-            {
-                var messageTask = await messageTaskRepository.FindAsync(x => x.Id == args.MessageTaskId, false);
-                if (messageTask == null) return;
-
-                Guid userId = Guid.Empty;
-                await messageTaskJobService.DisableJobAsync(messageTask.SchedulerJobId, userId);
-                return;
-            }
-            history.SetTaskId(args.TaskId);
-            var messageData = await domainService.GetMessageDataAsync(history.MessageTask, history.MessageTask.Variables);
-            history.SetSending();
-
-            if (!args.IsTest && !history.MessageTask.SendTime.HasValue)
-            {
-                history.MessageTask.SetSending();
-            }
-
-            await messageTaskHistoryRepository.UpdateAsync(history);
-
-            var channel = await channelRepository.FindAsync(x => x.Id == history.MessageTask.ChannelId);
-
-            var eto = channel.Type.GetSendMessageEvent(history.MessageTask.ChannelId.Value, messageData, history);
-            await eventBus.PublishAsync(eto);
+            var query = new ExecuteMessageTaskEvent(args.MessageTaskId, args.IsTest, args.JobId, args.TaskId);
+            await eventBus.PublishAsync(query);
         }
         finally
         {
             activity?.Dispose();
         }
-    }
-
-    private async Task<(IChannelRepository, IMessageTaskRepository, IMessageTaskHistoryRepository, IDomainEventBus, MessageTaskDomainService, IMessageTaskJobService, IUnitOfWork)> GetRequiredServiceAsync(IServiceProvider serviceProvider, string environment)
-    {
-        var multiEnvironmentSetter = serviceProvider.GetRequiredService<IMultiEnvironmentSetter>();
-        multiEnvironmentSetter.SetEnvironment(environment);
-        var channelRepository = serviceProvider.GetRequiredService<IChannelRepository>();
-        var messageTaskRepository = serviceProvider.GetRequiredService<IMessageTaskRepository>();
-        var messageTaskHistoryRepository = serviceProvider.GetRequiredService<IMessageTaskHistoryRepository>();
-        var eventBus = serviceProvider.GetRequiredService<IDomainEventBus>();
-        var domainService = serviceProvider.GetRequiredService<MessageTaskDomainService>();
-        var messageTaskJobService = serviceProvider.GetRequiredService<IMessageTaskJobService>();
-        var unitOfWork = serviceProvider.GetRequiredService<IUnitOfWork>();
-
-        return (channelRepository, messageTaskRepository, messageTaskHistoryRepository, eventBus, domainService, messageTaskJobService, unitOfWork);
     }
 }
