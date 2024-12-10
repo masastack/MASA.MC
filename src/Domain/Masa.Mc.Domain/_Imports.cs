320a2c7bd2ce42925dde8f58a0e6c82cea82190a
wzh425 <wuzihao425@outlook.com>
2024-12-10 14:34:22 +0800

    ⚡ refactor: Global using organization

--- src/Domain/Masa.Mc.Domain/_Imports.cs
+++ src/Domain/Masa.Mc.Domain/_Imports.cs
@@ -1,6 +1,13 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
+// System Namespaces
+global using System.Collections.Concurrent;
+global using System.Collections.ObjectModel;
+global using System.Linq.Expressions;
+global using System.Reflection;
+
+// MASA Building Blocks
 global using Masa.BuildingBlocks.Data;
 global using Masa.BuildingBlocks.Data.Contracts;
 global using Masa.BuildingBlocks.Ddd.Domain.Entities;
@@ -10,29 +17,34 @@ global using Masa.BuildingBlocks.Ddd.Domain.Events;
 global using Masa.BuildingBlocks.Ddd.Domain.Repositories;
 global using Masa.BuildingBlocks.Ddd.Domain.Values;
 global using Masa.BuildingBlocks.Dispatcher.Events;
+
+// MASA Stack SDKs and Contracts
 global using Masa.BuildingBlocks.StackSdks.Auth.Contracts.Model;
+
+// MASA MC Contracts and Consts
 global using Masa.Mc.Contracts.Admin.Consts;
 global using Masa.Mc.Contracts.Admin.Enums.MessageTemplates;
 global using Masa.Mc.Contracts.Admin.Enums.MessageTasks;
 global using Masa.Mc.Contracts.Admin.Enums.ReceiverGroups;
+
+// MASA MC Domain Aggregates
 global using Masa.Mc.Domain.Channels.Aggregates;
-global using Masa.Mc.Domain.Channels.Events;
 global using Masa.Mc.Domain.MessageInfos.Aggregates;
-global using ExtraPropertyDictionary = Masa.Mc.Data.ExtraPropertyDictionary;
 global using Masa.Mc.Domain.MessageRecords.Aggregates;
-global using Masa.Mc.Domain.MessageRecords.Events;
 global using Masa.Mc.Domain.MessageTasks.Aggregates;
-global using Masa.Mc.Domain.MessageTasks.Events;
 global using Masa.Mc.Domain.MessageTemplates.Aggregates;
-global using Masa.Mc.Domain.MessageTemplates.Events;
 global using Masa.Mc.Domain.ReceiverGroups.Aggregates;
 global using Masa.Mc.Domain.WebsiteMessages.Aggregates;
+
+// MASA MC Domain Events
+global using Masa.Mc.Domain.Channels.Events;
+global using Masa.Mc.Domain.MessageRecords.Events;
+global using Masa.Mc.Domain.MessageTasks.Events;
+global using Masa.Mc.Domain.MessageTemplates.Events;
 global using Masa.Mc.Domain.WebsiteMessages.Events;
+
+// MASA MC Infrastructure Helper
 global using Masa.Mc.Infrastructure.Common.Helper;
-global using System.Collections.Concurrent;
-global using System.Collections.ObjectModel;
-global using System.ComponentModel;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Linq.Expressions;
-global using System.Reflection;
+
+// MASA MC Data
 global using Masa.Mc.Data;
--- src/Infrastructure/Masa.Mc.EntityFrameworkCore/_Imports.cs
+++ src/Infrastructure/Masa.Mc.EntityFrameworkCore/_Imports.cs
@@ -1,41 +1,50 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-global using System.Collections.Concurrent;
-global using Masa.BuildingBlocks.Data;
-global using Masa.BuildingBlocks.Data.UoW;
-global using Masa.BuildingBlocks.Data.Contracts;
-global using Masa.Contrib.Ddd.Domain.Repository.EFCore;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-global using Microsoft.Extensions.Logging;
-global using Microsoft.EntityFrameworkCore.Design;
-global using Microsoft.Extensions.Configuration;
-global using System.Reflection;
-global using Masa.BuildingBlocks.Dispatcher.IntegrationEvents.Logs;
-global using Masa.Contrib.Dispatcher.IntegrationEvents.EventLogs.EFCore;
-global using Masa.Mc.Domain.Channels.Aggregates;
-global using Masa.Mc.Domain.Consts;
-global using Masa.Mc.Domain.MessageRecords.Aggregates;
-global using Masa.Mc.Domain.Channels.Repositories;
-global using Masa.Mc.Domain.MessageTemplates.Aggregates;
-global using Masa.Mc.EntityFrameworkCore;
+// System Namespaces
 global using System.Linq.Expressions;
-global using Masa.Mc.Domain.MessageTasks.Aggregates;
-global using Masa.Mc.Domain.MessageTasks.Repositories;
-global using Masa.Mc.Contracts.Admin.Enums.MessageTasks;
-global using Masa.Mc.Domain.MessageTemplates.Repositories;
-global using Masa.Mc.Domain.WebsiteMessages.Aggregates;
-global using Masa.Mc.Domain.WebsiteMessages.Repositories;
-global using Masa.Mc.Domain.ReceiverGroups.Aggregates;
-global using Masa.Mc.Domain.ReceiverGroups.Repositories;
-global using Microsoft.EntityFrameworkCore.ChangeTracking;
+global using System.Reflection;
 global using System.Text.Json;
+
+// Microsoft Namespaces
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.ChangeTracking;
 global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+// Third-party Libraries
+global using EFCore.BulkExtensions;
+
+// MASA Building Blocks and Contrib Packages
+global using Masa.BuildingBlocks.Data.Contracts;
+global using Masa.BuildingBlocks.Data.UoW;
+global using Masa.BuildingBlocks.Dispatcher.IntegrationEvents.Logs;
+global using Masa.Contrib.Ddd.Domain.Repository.EFCore;
+global using Masa.Contrib.Dispatcher.IntegrationEvents.EventLogs.EFCore;
+
+// MASA MC Domain Aggregates
+global using Masa.Mc.Domain.Channels.Aggregates;
 global using Masa.Mc.Domain.MessageInfos.Aggregates;
+global using Masa.Mc.Domain.MessageRecords.Aggregates;
+global using Masa.Mc.Domain.MessageTasks.Aggregates;
+global using Masa.Mc.Domain.MessageTemplates.Aggregates;
+global using Masa.Mc.Domain.ReceiverGroups.Aggregates;
+global using Masa.Mc.Domain.WebsiteMessages.Aggregates;
+
+// MASA MC Domain Repositories
+global using Masa.Mc.Domain.Channels.Repositories;
 global using Masa.Mc.Domain.MessageInfos.Repositories;
 global using Masa.Mc.Domain.MessageRecords.Repositories;
+global using Masa.Mc.Domain.MessageTasks.Repositories;
+global using Masa.Mc.Domain.MessageTemplates.Repositories;
+global using Masa.Mc.Domain.ReceiverGroups.Repositories;
+global using Masa.Mc.Domain.WebsiteMessages.Repositories;
+
+// MASA MC Contracts and Consts
+global using Masa.Mc.Domain.Consts;
+global using Masa.Mc.Contracts.Admin.Enums.MessageTasks;
+
+// MASA MC Entity Framework Core
+global using Masa.Mc.EntityFrameworkCore;
 global using Masa.Mc.EntityFrameworkCore.Repositories;
 global using Masa.Mc.EntityFrameworkCore.ValueConverters;
 global using Masa.Mc.Infrastructure.EntityFrameworkCore.ValueConverters;
-global using EFCore.BulkExtensions;
