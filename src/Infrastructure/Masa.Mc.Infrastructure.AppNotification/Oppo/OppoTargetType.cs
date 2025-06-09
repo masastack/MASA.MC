206316ddb68193066a2e884d264675ec9c050139
wzh425 <wuzihao425@outlook.com>
2025-06-09 09:44:25 +0800

    refactor: Use enumerations instead of hard coded numbers to improve readability and maintainability

---
+++ src/Infrastructure/Masa.Mc.Infrastructure.AppNotification/Oppo/OppoTargetType.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Mc.Infrastructure.AppNotification.Oppo;
+
+public enum OppoTargetType
+{
+    RegistrationId = 2,
+    Alias = 5,
+    Tag = 6
+}
