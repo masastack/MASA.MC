143b364e7cb49d3d3b4d07cbdfaaa51673d8e99f
wzh425 <wuzihao425@outlook.com>
2026-01-30 14:40:15 +0800

    fix: Correct filtering condition in ChannelStatisticsQueryHandler (#658)
    * refactor: Optimize filtering logic in ChannelStatisticsQueryHandler
    
    - Replaced multiple Where clauses with a single expression-based condition for improved readability and maintainability.
    - Enhanced filtering capabilities by using a more flexible approach to handle optional input parameters.
    
    * fix: Correct filtering condition in ChannelStatisticsQueryHandler
    
    - Updated the end time condition in the filtering logic to ensure it correctly checks if the SendTime or ExpectSendTime is less than or equal to the input EndTime, improving the accuracy of date range queries.
    
    * fix: Remove unnecessary line in ChannelStatisticsQueryHandler
    
    - Eliminated an empty line in the filtering logic of ChannelStatisticsQueryHandler to enhance code cleanliness and maintainability.

--- src/Services/Masa.Mc.Service/Application/ChannelStatistics/ChannelStatisticsQueryHandler.cs
+++ src/Services/Masa.Mc.Service/Application/ChannelStatistics/ChannelStatisticsQueryHandler.cs
@@ -214,8 +214,7 @@ public class ChannelStatisticsQueryHandler
         condition = condition.And(input.ChannelId.HasValue, x => x.ChannelId == input.ChannelId);
         condition = condition.And(input.TemplateId.HasValue, x => x.MessageEntityType == MessageEntityTypes.Template && x.MessageEntityId == input.TemplateId);
         condition = condition.And(input.StartTime.HasValue, x => (x.SendTime ?? x.ExpectSendTime) >= input.StartTime);
-        condition = condition.And(input.EndTime.HasValue, x => (x.SendTime ?? x.ExpectSendTime) >= input.EndTime);
-
+        condition = condition.And(input.EndTime.HasValue, x => (x.SendTime ?? x.ExpectSendTime) <= input.EndTime);
         return query.Where(condition);
     }
 
