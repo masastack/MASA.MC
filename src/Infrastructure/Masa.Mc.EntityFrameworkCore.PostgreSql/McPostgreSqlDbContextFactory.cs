3a4c200901843e234623a6d619f6d7d5d3edcf0d
wzh425 <wuzihao425@outlook.com>
2026-08-15 15:12:11 +0800

    Update configuration handling by adding support for secrets files and removing hardcoded connection strings in PostgreSQL and SQL Server settings. Enhance DbContextFactory classes to include optional secrets configuration and environment variables for improved security and flexibility.

--- src/Infrastructure/Masa.Mc.EntityFrameworkCore.PostgreSql/McPostgreSqlDbContextFactory.cs
+++ src/Infrastructure/Masa.Mc.EntityFrameworkCore.PostgreSql/McPostgreSqlDbContextFactory.cs
@@ -12,6 +12,8 @@ public class McPostgreSqlDbContextFactory : IDesignTimeDbContextFactory<McDbCont
         var configurationBuilder = new ConfigurationBuilder();
         var configuration = configurationBuilder
             .AddJsonFile("appsettings.PostgreSql.json")
+            .AddJsonFile("appsettings.secrets.json", optional: true)
+            .AddEnvironmentVariables()
             .Build();
         optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection")!, b => b.MigrationsAssembly("Masa.Mc.EntityFrameworkCore.PostgreSql"));
 
